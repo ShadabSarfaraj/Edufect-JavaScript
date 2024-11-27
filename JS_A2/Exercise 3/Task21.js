36a4b344a3f95bb31dc5b81ed50f5168741bed91
ShadabSarfaraj <shadabsarfaraj32@gmail.com>
2024-11-28 02:30:43 +0530

    Completed task 21.1 and 21.2

--- JS_A2/Exercise 3/Task21.js
+++ JS_A2/Exercise 3/Task21.js
@@ -13,15 +13,43 @@ let continentArray = [
   { city: "Delhi", continent: "Asia" },
   { city: "Seattle", continent: "North America" },
   { city: "Paris", continent: "Europe" },
-  { city: "New York", continent: " North America" },
+  { city: "New York", continent: "North America" },
   { city: "Rome", continent: "Europe" },
   { city: "Bengaluru", continent: "Asia" },
   { city: "Los Angeles", continent: "North America" },
 ];
 
-const continent = "Asia";
+const continent = "North America";
+const copy1 = [...personArray];
 
-function getPersonByContinent(personArray,continentArray,continent){
+// * Task 21.1
 
+function getPersonByContinent(personArray, continentArray, continent) {
+  let filteredArray = personArray.filter(function (person) {
+    const city = person.city;
+    let continentRecord = continentArray.find(function (record) {
+      return city == record.city && record.continent == continent;
+    });
+    return continentRecord != undefined;
+  });
+  // console.log(filteredArray);
 }
-getPersonByContinent(personArray,continentArray,continent);
+getPersonByContinent(personArray, continentArray, continent);
+
+// * Task21.2
+function sortPersonByContinent(personArray, continentArray) {
+  personArray.sort(sortPersonAsc);
+  function sortPersonAsc(personA, personB) {
+    let isFind1 = continentArray.find(function (record) {
+      return record.city == personA.city;
+    });
+    let isFind2 = continentArray.find(function (record) {
+      return record.city == personB.city;
+    });
+
+    if (isFind1 != undefined && isFind2 != undefined)
+      return isFind1.continent.localeCompare(isFind2.continent);
+  }
+}
+sortPersonByContinent(copy1, continentArray);
+console.log(copy1);
